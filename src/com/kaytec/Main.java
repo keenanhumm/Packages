9437499f827e9747043506e455177a57dccfaf53
keenanhumm <keenan.humm@huskers.unl.edu>
2019-08-15 22:55:26 -0500

    my window impl

--- src/com/kaytec/Main.java
+++ src/com/kaytec/Main.java
@@ -3,6 +3,7 @@ package com.kaytec;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MyWindow myWindow = new MyWindow("To Mars");
+        myWindow.setVisible(true);
     }
 }
